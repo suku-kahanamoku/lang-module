77af63079a67c818061c625d82d24a1213f64c3b
Suche Kahanamoku <sukusovi@gmail.com>
2025-07-19 11:37:28 +0200

    first commit

---
+++ playground/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  modules: ["../src/module"],
+  langModule: {},
+  devtools: { enabled: true },
+});
---
+++ test/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,5 @@
+import LangModule from "../../../src/module";
+
+export default defineNuxtConfig({
+  modules: [LangModule],
+});
