07b753b919f4a70a8cac0501ec0d25949b6533ac
Suche Kahanamoku <sukusovi@gmail.com>
2025-07-20 17:23:37 +0200

    Update

--- playground/app.vue
+++ playground/app.vue
@@ -2,10 +2,17 @@
 const {
   i18n: { locale },
 } = useLang();
+const localePath = useLocalePath();
 </script>
 <template>
   <div>Nuxt module playground!</div>
   <div>{{ $tt("$.btn.submit") }}</div>
+  <div>
+    {{ locale }}
+  </div>
+  <div>
+    {{ localePath("/") }}
+  </div>
 </template>
 
 <script setup></script>
